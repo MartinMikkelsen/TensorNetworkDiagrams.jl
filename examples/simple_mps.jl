37b9e0a118f57b4eaea6ac51bf0e4d5055454f84
MartinMikkelsen <MartinMikkelsen@protonmail.com>
2025-11-27 20:43:14 +0100

    added MPS/MPO

--- examples/simple_mps.jl
+++ examples/simple_mps.jl
@@ -1,10 +1,13 @@
 using .TensorNetworkDiagrams
 
-mps = MPS(length = 4)
+mps = MPS(length = 4, boundary_labels = ("", ""))
 tikz = to_tikz(mps)
 
 save_tex(tikz, "examples/mps.tex")
 
-mpo = MPO(4)
+mpo = MPO(5, boundary_labels = ("", ""))
 tikz_mpo = to_tikz(mpo)
 save_tex(tikz_mpo, "examples/mpo.tex")
+
+tikz_TN = to_tikz(mps, mpo)
+save_tex(tikz_TN, "examples/tensornetwork.tex", transparent=false)
--- src/TensorNetworkDiagrams.jl
+++ src/TensorNetworkDiagrams.jl
@@ -97,8 +97,10 @@ function to_tikz(mps::MPS)
     io = IOBuffer()
     println(io, "\\begin{tikzpicture}")
 
-    # Draw left boundary bond
-    println(io, "  \\draw[black] (-$(mps.node_spacing),0) -- node [label={[shift={(0,-0.15)}]\$$(mps.boundary_labels[1])\$}] {} (0,0);")
+    # Draw left boundary bond (only if label is non-empty)
+    if !isempty(mps.boundary_labels[1])
+        println(io, "  \\draw[black] (-$(mps.node_spacing),0) -- node [label={[shift={(0,-0.15)}]\$$(mps.boundary_labels[1])\$}] {} (0,0);")
+    end
 
     # Draw internal bonds
     for i in 1:(mps.length - 1)
@@ -106,9 +108,11 @@ function to_tikz(mps::MPS)
         println(io, "  \\draw[black] ($x,0) -- node [label={[shift={(0,-0.15)}]\$$(mps.bond_labels[i])\$}] {} ++ ($(mps.node_spacing),0);")
     end
 
-    # Draw right boundary bond
-    x = (mps.length - 1) * mps.node_spacing
-    println(io, "  \\draw[black] ($x,0) -- node [label={[shift={(0,-0.15)}]\$$(mps.boundary_labels[2])\$}] {} ++ ($(mps.node_spacing),0);")
+    # Draw right boundary bond (only if label is non-empty)
+    if !isempty(mps.boundary_labels[2])
+        x = (mps.length - 1) * mps.node_spacing
+        println(io, "  \\draw[black] ($x,0) -- node [label={[shift={(0,-0.15)}]\$$(mps.boundary_labels[2])\$}] {} ++ ($(mps.node_spacing),0);")
+    end
 
     # Draw physical legs
     for i in 0:(mps.length - 1)
@@ -131,8 +135,10 @@ function to_tikz(mpo::MPO)
     io = IOBuffer()
     println(io, "\\begin{tikzpicture}")
 
-    # Draw left boundary bond
-    println(io, "  \\draw[black] (-$(mpo.node_spacing),0) -- node [label={[shift={(0,-0.15)}]\$$(mpo.boundary_labels[1])\$}] {} (0,0);")
+    # Draw left boundary bond (only if label is non-empty)
+    if !isempty(mpo.boundary_labels[1])
+        println(io, "  \\draw[black] (-$(mpo.node_spacing),0) -- node [label={[shift={(0,-0.15)}]\$$(mpo.boundary_labels[1])\$}] {} (0,0);")
+    end
 
     # Draw internal bonds
     for i in 1:(mpo.length - 1)
@@ -140,9 +146,11 @@ function to_tikz(mpo::MPO)
         println(io, "  \\draw[black] ($x,0) -- node [label={[shift={(0,-0.15)}]\$$(mpo.bond_labels[i])\$}] {} ++ ($(mpo.node_spacing),0);")
     end
 
-    # Draw right boundary bond
-    x = (mpo.length - 1) * mpo.node_spacing
-    println(io, "  \\draw[black] ($x,0) -- node [label={[shift={(0,-0.15)}]\$$(mpo.boundary_labels[2])\$}] {} ++ ($(mpo.node_spacing),0);")
+    # Draw right boundary bond (only if label is non-empty)
+    if !isempty(mpo.boundary_labels[2])
+        x = (mpo.length - 1) * mpo.node_spacing
+        println(io, "  \\draw[black] ($x,0) -- node [label={[shift={(0,-0.15)}]\$$(mpo.boundary_labels[2])\$}] {} ++ ($(mpo.node_spacing),0);")
+    end
 
     # Draw upper physical legs
     for i in 0:(mpo.length - 1)
@@ -166,10 +174,103 @@ function to_tikz(mpo::MPO)
     return String(take!(io))
 end
 
-function save_tex(tikz_code::String, filename::String; standalone = true, cleanup = true)
+function to_tikz(mps::MPS, mpo::MPO; vertical_spacing::Float64 = 0.7)
+    io = IOBuffer()
+    println(io, "\\begin{tikzpicture}")
+
+    mps_y = vertical_spacing
+    mpo_y = 0.0
+    overlap = min(mps.length, mpo.length)
+
+    # Draw MPS horizontal bonds
+    if !isempty(mps.boundary_labels[1])
+        println(io, "  \\draw[black] (-$(mps.node_spacing),$mps_y) -- (0,$mps_y);")
+    end
+
+    for i in 1:(mps.length - 1)
+        x = (i - 1) * mps.node_spacing
+        label = mps.bond_labels[i]
+        if !isempty(label)
+            println(io, "  \\draw[black] ($x,$mps_y) -- node [label={[shift={(0,-0.15)}]\$$label\$}] {} ++ ($(mps.node_spacing),0);")
+        else
+            println(io, "  \\draw[black] ($x,$mps_y) -- ++ ($(mps.node_spacing),0);")
+        end
+    end
+
+    if !isempty(mps.boundary_labels[2])
+        x = (mps.length - 1) * mps.node_spacing
+        println(io, "  \\draw[black] ($x,$mps_y) -- ++ ($(mps.node_spacing),0);")
+    end
+
+    # Draw MPO horizontal bonds
+    if !isempty(mpo.boundary_labels[1])
+        println(io, "  \\draw[black] (-$(mpo.node_spacing),$mpo_y) -- (0,$mpo_y);")
+    end
+
+    for i in 1:(mpo.length - 1)
+        x = (i - 1) * mpo.node_spacing
+        label = mpo.bond_labels[i]
+        if !isempty(label)
+            println(io, "  \\draw[black] ($x,$mpo_y) -- node [label={[shift={(0,-0.15)}]\$$label\$}] {} ++ ($(mpo.node_spacing),0);")
+        else
+            println(io, "  \\draw[black] ($x,$mpo_y) -- ++ ($(mpo.node_spacing),0);")
+        end
+    end
+
+    if !isempty(mpo.boundary_labels[2])
+        x = (mpo.length - 1) * mpo.node_spacing
+        println(io, "  \\draw[black] ($x,$mpo_y) -- ++ ($(mpo.node_spacing),0);")
+    end
+
+    # Draw contracted vertical legs (between MPS and MPO)
+    for i in 0:(overlap - 1)
+        x = i * mps.node_spacing
+        println(io, "  \\draw[black] ($x,$mpo_y) -- ($x,$mps_y);")
+    end
+
+    # Draw uncontracted MPS physical legs (upper)
+    for i in overlap:(mps.length - 1)
+        x = i * mps.node_spacing
+        label = mps.physical_labels[i + 1]
+        if !isempty(label)
+            println(io, "  \\draw[black] ($x,$mps_y) -- node [label={[shift={(0,0.2)}]\$$label\$}] {} ++ (0,$vertical_spacing);")
+        else
+            println(io, "  \\draw[black] ($x,$mps_y) -- ++ (0,$vertical_spacing);")
+        end
+    end
+
+    # Draw MPO lower physical legs
+    for i in 0:(mpo.length - 1)
+        x = i * mpo.node_spacing
+        label = mpo.lower_physical_labels[i + 1]
+        if !isempty(label)
+            println(io, "  \\draw[black] ($x,$mpo_y) -- node [label={[shift={(0,-1.0)}]\$$label\$}] {} ++ (0,-$vertical_spacing);")
+        else
+            println(io, "  \\draw[black] ($x,$mpo_y) -- ++ (0,-$vertical_spacing);")
+        end
+    end
+
+    # Draw MPS nodes
+    for i in 0:(mps.length - 1)
+        x = i * mps.node_spacing
+        println(io, "  \\node[$(mps.node_style)] at ($x,$mps_y){};")
+    end
+
+    # Draw MPO nodes
+    for i in 0:(mpo.length - 1)
+        x = i * mpo.node_spacing
+        println(io, "  \\node[$(mpo.node_style)] at ($x,$mpo_y){};")
+    end
+
+    println(io, "\\end{tikzpicture}")
+    return String(take!(io))
+end
+
+function save_tex(tikz_code::String, filename::String; standalone=true, cleanup=true, transparent=false)
     tex_content = if standalone
+        bg_option = transparent ? ",transparent" : ""
         """
-        \\documentclass[tikz,border=2mm]{standalone}
+        \\documentclass[tikz,border=2mm$bg_option]{standalone}
         \\begin{document}
         $tikz_code
         \\end{document}
@@ -177,12 +278,12 @@ function save_tex(tikz_code::String, filename::String; standalone = true, cleanu
     else
         tikz_code
     end
-
+    
     write(filename, tex_content)
     println("✓ Saved to $filename")
     println("  Compile with: tectonic $filename")
-
-    return if cleanup
+    
+    if cleanup
         base = replace(filename, ".tex" => "")
         aux_extensions = [".aux", ".log", ".fls", ".fdb_latexmk", ".synctex.gz"]
         println("  Then clean up with: rm $base.{aux,log,fls,fdb_latexmk,synctex.gz}")
